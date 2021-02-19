019ffa10647f9d31381676779ab4d1a091a42209
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-02-19 15:13:38 +0100

    show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,49 +1,27 @@
 @extends('layouts.main')
 
-@section('header')
-  <h1 class="mt-5">Dettaglio post</h1>
-@endsection
 
 @section('content')
-  <table class="table table-dark table-striped table-bordered">
-    @foreach ($post->getAttributes() as $key => $value)
-    <tr>
-      <td> <strong>{{ $key }}</strong> </td>
-      <td> {{ $value }}</td>
-    </tr>
-    @endforeach
-    <table class="table table-light table-striped table-bordered">
-			<h3>Post Info</h3>
-        <thead>
-          <tr>
-            <th>Post status:</th>
-            <th>Comment status:</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td> {{ $post->infoPost->post_status }}</td>
-            <td> {{ $post->infoPost->comment_status }}</td>
-          </tr>
-        </tbody>
-    </table>
-    @if($post->infoPost->post_status == 'private')
-    <a href=""class="btn btn-lg btn-dark">Pubblica post</a>
-    @elseif($post->infoPost->post_status == 'draft')
-    <a href=""class="btn btn-lg btn-dark">modifica post</a>
-    @endif
-
+  <div class="text-center my-4">
+    <h1>{{ $post->title }}</h1>
+    <h3> {{ $post->subtitle }}</h3>
+    <small> {{$post->author}} - {{$post->publication_date}} - {{ $post->infoPost->post_status }} - {{$post->infoPost->comment_status }}</small>
+    <p class="mt-4"> {{ $post->text }}</p>
+    <div class="text-right">
+      <a href="{{ route('posts.edit',$post->id)}}"class="btn btn-lg btn-dark">Modifica post</a>
+    </div>
+  </div>
     @if($post->infoPost->post_status == 'public')
     <div>
-      <h2 class="mt-5">Comments</h2>
+      <h3 class="my-5">Comments</h3>
       @foreach($post->comments as $comment)
       <div>
-        <p> {{ $comment->author }}</p>
+        <h5> <i class="fas fa-user"></i> {{ $comment->author }}</h5>
         <p> {{ $comment->text }}</p>
+        <small>{{ $comment->created_at }}</small>
         <hr>
       @endforeach
       </div>
-
     <div class="my-5">
       <form action="{{ route('comment.store', ['post_id' => $post->id]) }}" method="POST">
         @csrf
@@ -63,7 +41,7 @@
 @endsection
 
 @section('footer')
-<div class="text-right">
-  <a href="{{ route('posts.index')}}"class="btn btn-lg btn-dark">Torna all'elenco</a>
-</div>
+  <div class="container text-right">
+    <a href="{{ route('posts.index')}}"class="btn btn-lg btn-dark">Torna all'elenco</a>
+  </div>
 @endsection
